b64be9b18624235ca948d872dfa7af21511b9bfc
Rocha <jrocha@proforma.com>
2018-08-05 23:42:32 -0400

    Almost finished Explorer.

--- blackjack/public/main.js
+++ blackjack/public/main.js
@@ -44,6 +44,58 @@ const hit = () => {
   document.querySelector('.Display').appendChild(_li)
 }
 
+//Dealer
+const dDeck = []
+
+const createDDeck = () => {
+
+  const dSuits = [" of Hearts", " of Clubs", " of Diamonds", " of Spades"]
+  const dRanks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+  const dCard_values = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+  for (let s = 0; s < dSuits.length; s++) {
+    for (let r = 0; r < dRanks.length; r ++) {
+      dDeck.push({
+  	dSuit: dSuits[s],
+    dRank: dRanks[r],
+  	dValue: dCard_values[r]
+       })
+     }
+  }
+
+  const displayDCard = () => {
+    const displayDCard = dDeck.pop()
+    console.log(displayDCard)
+  // create the elemnet
+  const _li = document.createElement("li")
+  // set all the properties
+  _li.textContent = displayDCard.dRank + displayDCard.dSuit
+  // insert into a parent
+  document.querySelector('.Dealer').appendChild(_li)
+  }
+
+  const displayDCard2 = () => {
+    const displayDCard2 = dDeck.pop()
+    console.log(displayDCard2)
+    const _li = document.createElement("li")
+    // set all the properties
+    _li.textContent = displayDCard2.dRank + displayCard2.dSuit
+    // insert into a parent
+    document.querySelector('.Dealer').appendChild(_li)
+  }
+
+  const dHit = () => {
+    const dHit = dDeck.pop()
+    console.log(dHit)
+    const _li = document.createElement("li")
+    // set all the properties
+    _li.textContent = dHit.dRank + dHit.dSuit
+    // insert into a parent
+    document.querySelector('.Dealer').appendChild(_li)
+  }
+
+
+//Player
 const deck = []
 
 const createDeck = () => {
@@ -66,9 +118,11 @@ const createDeck = () => {
 }
 
 
-
 document.addEventListener('DOMContentLoaded', createDeck)
 document.querySelector('.Deal').addEventListener('click', displayCard)
 document.querySelector('.Deal').addEventListener('click', displayCard2)
+document.querySelector('.Deal').addEventListener('click', displayDCard)
+document.querySelector('.Deal').addEventListener('click', displayDCard2)
 document.querySelector('.hit').addEventListener('click', hit)
+document.querySelector('.hit').addEventListener('click', dHit)
 document.querySelector('.shuffleButton').addEventListener('click', shuffleDeck)
